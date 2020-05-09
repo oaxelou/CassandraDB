f13d26dce26cec4af3b5f7133f7fee4a9beb9dd7
Olympia <oaxelou@uth.gr>
2020-05-09 08:20:06 +0300

    Script πως να χρησιμοποιηθεί ο converter

---
+++ Database_stuff/convert_json_to_csv/script.sh
@@ -0,0 +1,6 @@
+# Trexei se python2
+# gia na trexei prepei na einai ston idio fakelo to json arxeio kai to txt me ta columns
+python converter.py business.json
+python converter.py reviews.json
+python converter.py tips.json
+python converter.py users.json
