e1d7e75b59e86810b1bbe03c7a85b6c9a4cae5db
Olympia <oaxelou@uth.gr>
2020-05-09 08:54:56 +0300

    script to ensure the validity of stars & review_count

---
+++ Database_stuff/set_stars_review_count_.py
@@ -0,0 +1,30 @@
+from cassandra.cluster import Cluster
+import cassandra 
+
+cluster = Cluster()
+session = cluster.connect('recommendations_keyspace')
+
+query_str="select businessid, city, stars from business_by_id;"
+rowRes=session.execute(query_str);
+for row in rowRes:
+	counted_stars=0.0
+	counted_reviews=0
+	query_str="select stars from review_by_businessid where businessid='" + row.businessid + "';"
+	rowRes_review=session.execute(query_str);
+	for row_review in rowRes_review:
+		counted_stars+=row_review.stars
+		counted_reviews+=1
+	if counted_reviews>0:
+		counted_stars=counted_stars/float(counted_reviews)
+	else:
+		counted_stars=0
+	# print row.businessid, ":", str(row.stars), "vs", str(0.5*round(counted_stars/0.5))
+	try:
+		if row.stars != (0.5*round(counted_stars/0.5)):
+			session.execute("update business_by_id set stars="+str(0.5*round(counted_stars/0.5))+" where businessid='"+row.businessid+"';")
+			session.execute("update business_by_city set stars="+str(0.5*round(counted_stars/0.5))+" where city='"+row.city+"' and businessid='"+row.businessid+"';")
+		session.execute("update business_by_id set review_count="+str(counted_reviews)+" where businessid='"+row.businessid+"';")
+		session.execute("update business_by_city set review_count="+str(counted_reviews)+" where city='"+row.city+"' and businessid='"+row.businessid+"';")
+	except cassandra.protocol.SyntaxException as e:
+		print "Going to ignore", row.businessid, "from", row.city
+
